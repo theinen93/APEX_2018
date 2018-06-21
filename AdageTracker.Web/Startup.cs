ca78115b4890150ea6dde375793579d483b74dec
theinen <theinen@adadgetechnologies.com>
2018-06-21 17:14:32 -0500

    Initial Commit

---
+++ AdageTracker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdageTracker.Web.Startup))]
+namespace AdageTracker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
